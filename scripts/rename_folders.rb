c6e2560e8102eac0ab88650d4ad6471d08cfec21
Barry Hess <barry@bjhess.com>
2022-05-06 13:24:51 -0500

    Rename 2022 blog posts with dashes.

--- scripts/rename_folders.rb
+++ scripts/rename_folders.rb
@@ -1,7 +1,7 @@
 #! ruby
 
 puts "Processing files"
-Dir.glob('blog/2021/*').sort.each do |folder_name|
+Dir.glob('blog/2022/*').sort.each do |folder_name|
   puts "Renaming #{folder_name}"
   new_folder_name = folder_name.gsub('_', '-')
   File.rename(folder_name, new_folder_name)
